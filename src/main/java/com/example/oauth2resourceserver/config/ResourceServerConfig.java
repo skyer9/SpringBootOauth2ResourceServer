627ff69f76c3d14be7240dd94701a65c547d6978
skyer9 <skyer9@gmail.com>
2021-07-25 14:41:42 +0900

    설명추가

--- src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // key-uri: http://auth.localhost:9000/oauth/token_key
+    // 위 설정으로 인해, JWT 토큰인것과, 암호화방식, 그리고 공개키까지 제공됩니다.
+    // 따라서, 추가설정 없이 암호화 JWT 토큰을 이용할 수 있습니다.
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
