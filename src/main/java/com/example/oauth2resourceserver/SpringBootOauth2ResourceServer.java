c5ed2a7d1c132021fe0f2b46ccd73a4dd2c7649a
skyer9 <skyer9@gmail.com>
2021-07-31 11:00:28 +0900

    리소스 서버로 변경

--- src/main/java/com/example/oauth2resourceserver/SpringBootOauth2ResourceServer.java
+++ src/main/java/com/example/oauth2resourceserver/SpringBootOauth2ResourceServer.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2resourceserverApplication {
+public class SpringBootOauth2ResourceServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oauth2resourceserverApplication.class, args);
+        SpringApplication.run(SpringBootOauth2ResourceServer.class, args);
     }
-
 }
